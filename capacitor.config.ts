4d8fb89fd199cc3f7e0e1c71a48db3583298cb60
vickizulianto <vickyzulianto704@gmail.com>
2026-05-14 22:08:10 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tugas2App',
+  webDir: 'dist'
+};
+
+export default config;
